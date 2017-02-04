61957db2a7ea67ba882beb88108ef60afdf8cabe
chechaluque <cesarluque2000@gmail.com>
2017-02-04 12:49:34 -0800

    Fix Comment t Message View File

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    @message = Message.find(params[:message])
+    @message = Message.find(params[:message_id])
     @comment = @message.comments.create(comment_params)
     @comment.user_id = current_user.id
 
-    if comment.save
-      redirec_to message_path(@message)
+    if @comment.save
+      redirect_to message_path(@message)
     else
       render 'new'
     end
